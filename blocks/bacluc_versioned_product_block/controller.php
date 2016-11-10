f2fe583f2890653bd60035938179305f009d3bd8
BacLuc <lucius.bachmann@gmx.ch>
2016-11-10 15:52:58 +0100

    versioned product is working

---
+++ blocks/bacluc_versioned_product_block/controller.php
@@ -0,0 +1,170 @@
+<?php
+namespace Concrete\Package\BaclucInvoicePackage\Block\BaclucVersionedProductBlock;
+
+use Concrete\Core\Package\Package;
+use Concrete\Package\BaclucAccountingPackage\Src\Account;
+use Concrete\Package\BaclucEventPackage\Src\Event;
+use Concrete\Package\BaclucInvoicePackage\Src\VersionedProduct;
+use Concrete\Package\BaclucProductPackage\Src\Product;
+use Concrete\Package\BasicTablePackage\Src\BlockOptions\DropdownBlockOption;
+use Concrete\Package\BasicTablePackage\Src\BlockOptions\TableBlockOption;
+use Concrete\Core\Block\BlockController;
+use Concrete\Package\BasicTablePackage\Src\BasicTableInstance;
+use Concrete\Package\BasicTablePackage\Src\BlockOptions\TextBlockOption;
+use Concrete\Package\BasicTablePackage\Src\BaseEntity;
+use Concrete\Package\BasicTablePackage\Src\ExampleBaseEntity;
+use Core;
+use Concrete\Package\BasicTablePackage\Src\BlockOptions\CanEditOption;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\ORM\QueryBuilder;
+use OAuth\Common\Exception\Exception;
+use Page;
+use User;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\Field as Field;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\SelfSaveInterface as SelfSaveInterface;
+use Loader;
+
+use Concrete\Package\BasicTablePackage\Block\BasicTableBlockPackaged\Test as Test;
+
+class Controller extends \Concrete\Package\BasicTablePackage\Block\BasicTableBlockPackaged\Controller
+{
+    protected $btHandle = 'bacluc_versioned_product_block';
+    /**
+     * table title
+     * @var string
+     */
+    protected $header = "BaclucProductBlock";
+
+    /**
+     * Array of \Concrete\Package\BasicTablePackage\Src\BlockOptions\TableBlockOption
+     * @var array
+     */
+    protected $requiredOptions = array();
+
+    /**
+     * @var \Concrete\Package\BasicTablePackage\Src\BaseEntity
+     */
+    protected $model;
+
+
+    /**
+     * set blocktypeset
+     * @var string
+     */
+    protected $btDefaultSet = 'bacluc_product_set';
+
+    /**
+     *
+     * Controller constructor.
+     * @param null $obj
+     */
+    function __construct($obj = null)
+    {
+        //$this->model has to be instantiated before, that session handling works right
+
+        $this->model = new VersionedProduct();
+        parent::__construct($obj);
+
+
+
+        if ($obj instanceof Block) {
+         $bt = $this->getEntityManager()->getRepository('\Concrete\Package\BasicTablePackage\Src\BasicTableInstance')->findOneBy(array('bID' => $obj->getBlockID()));
+
+            $this->basicTableInstance = $bt;
+        }
+
+
+/*
+ * add blockoptions here if you wish
+        $this->requiredOptions = array(
+            new TextBlockOption(),
+            new DropdownBlockOption(),
+            new CanEditOption()
+        );
+
+        $this->requiredOptions[0]->set('optionName', "Test");
+        $this->requiredOptions[1]->set('optionName', "TestDropDown");
+        $this->requiredOptions[1]->setPossibleValues(array(
+            "test",
+            "test2"
+        ));
+
+        $this->requiredOptions[2]->set('optionName', "testlink");
+*/
+
+
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getBlockTypeDescription()
+    {
+        return t("Create, Edit or Delete Products");
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockTypeName()
+    {
+        return t("Bacluc Product");
+    }
+
+
+    /**
+     * if save is pressed, the data is saved to the sql table
+     * @throws \Exception
+     */
+    function action_save_row($redirectOnSuccess = true)
+    {
+
+
+
+        if ($this->post('rcID')) {
+            // we pass the rcID through the form so we can deal with stacks
+            $c = Page::getByID($this->post('rcID'));
+        } else {
+            $c = $this->getCollectionObject();
+        }
+        //form view is over
+        $v =  $this->checkPostValues();
+        if($v === false){
+            return false;
+        }
+
+        if ($this->editKey == null) {
+            $model = $this->model;
+        } else {
+            $oldmodel = $this->getEntityManager()->getRepository(get_class($this->model))->findOneBy(array($this->model->getIdFieldName() => $this->editKey));
+            $model = new VersionedProduct();
+            $oldmodel->set("NewVersion", $model);
+            $this->getEntityManager()->persist($oldmodel);
+        }
+
+        if($this->persistValues($model, $v) === false){
+            return false;
+        }
+
+        $this->getEntityManager()->flush();
+
+
+        $this->finishFormView();
+        if($redirectOnSuccess) {
+            $this->redirect($c->getCollectionPath());
+        }
+
+
+    }
+
+    public function addFilterToQuery(QueryBuilder $query, array $queryConfig = array())
+    {
+        $entityStart = $queryConfig['NewVersion']['shortname'];
+        $query->andWhere($query->expr()->isNull($entityStart));
+        return $query;
+    }
+
+
+}
---
+++ src/VersionedProduct.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucius
+ * Date: 01.02.16
+ * Time: 23:08
+ */
+namespace Concrete\Package\BaclucInvoicePackage\Src;
+use Concrete\Core\Html\Object\Collection;
+use Concrete\Package\BaclucProductPackage\Src\Product;
+use Concrete\Package\BasicTablePackage\Src\BaseEntity;
+use Concrete\Package\BasicTablePackage\Src\EntityGetterSetter;
+use Concrete\Package\BasicTablePackage\Src\Exceptions\ConsistencyCheckException;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\DateField as DateField;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\DirectEditAssociatedEntityField;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\DropdownField;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\FileField as FileField;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\WysiwygField as WysiwygField;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Expr\Expression;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\PersistentCollection;
+use Doctrine\ORM\Query\Expr;
+use Concrete\Package\BasicTablePackage\Src\FieldTypes\DropdownLinkField;
+use Concrete\Package\BaclucPersonPackage\Src\Address;
+use Concrete\Package\BaclucPersonPackage\Src\PostalAddress;
+use Concrete\Core\Package\Package;
+
+
+/*because of the hack with @DiscriminatorEntry Annotation, all Doctrine Annotations need to be
+properly imported*/
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\InheritanceType;
+use Doctrine\ORM\Mapping\DiscriminatorColumn;
+use Concrete\Package\BasicTablePackage\Src\DiscriminatorEntry\DiscriminatorEntry;
+use Doctrine\ORM\Mapping\Table;
+
+/**
+ * Class VersionedProduct
+ * Package  Concrete\Package\BaclucProductPackage\Src
+ * @DiscriminatorColumn(name="discr", type="string")
+ * @DiscriminatorEntry(value="Concrete\Package\BaclucInvoicePackage\Src\VersionedProduct")
+ * @Entity
+@Table(name="bacluc_versioned_product"
+)
+ *
+ */
+class VersionedProduct extends Product
+{
+    use EntityGetterSetter;
+    /**
+     * @var VersionedProduct
+     * @ManyToOne(targetEntity="Concrete\Package\BaclucInvoicePackage\Src\VersionedProduct")
+     */
+    protected $NewVersion;
+
+
+
+
+
+
+
+
+
+    public function __construct(){
+        parent::__construct();
+
+
+
+        if($this->OldVersions == null){
+            $this->OldVersions = new ArrayCollection();
+        }
+        $this->setDefaultFieldTypes();
+    }
+    public function setDefaultFieldTypes(){
+        parent::setDefaultFieldTypes();
+
+        $this->fieldTypes['NewVersion'] = new DropdownLinkField("NewVersion", "Old Version of","postNewVersion");
+        $this->fieldTypes['NewVersion']->setLinkInfo($this,"NewVersion",get_class($this),null,static::getDefaultGetDisplayStringFunction() );
+        $this->fieldTypes['NewVersion']->setShowInForm(false);
+    }
+
+
+    /**
+     * Returns the function, which generates the String for LInk Fields to identify the instance. Has to be unique to prevent errors
+     * @return \Closure
+     */
+    public static function getDefaultGetDisplayStringFunction(){
+        $function = function(VersionedProduct $item){
+            $item = BaseEntity::getBaseEntityFromProxy($item);
+            $returnString = '';
+            if(strlen($item->code) >0){
+                $returnString.= $item->code." ";
+            }
+            if(strlen($item->name) >0){
+                $returnString.= $item->name." ";
+            }
+            if(strlen($item->price) !=0){
+                $returnString.= $item->price." ";
+            }
+            return $returnString;
+        };
+        return $function;
+    }
+
+
+
+
+
+}
